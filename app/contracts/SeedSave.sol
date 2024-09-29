26b467f252efe8381dae5029ebb3746443d539af
Tincho Develops <97058453+tinchopapa56@users.noreply.github.com>
2024-09-29 15:26:18 -0300

    contract done

--- app/contracts/SeedSave.sol
+++ app/contracts/SeedSave.sol
@@ -58,19 +58,52 @@ contract SeedSale is Ownable{
     }
 
     function setCurrency (address newCurrency) public onlyOwner(){
-        require(seedStatus, "seedStatus Inactive");
+        require(!seedStatus, "seedStatus Inactive");
         currency = IERC20(newCurrency);
+        //emit
     }
-    function setSeedStatus(bool _status) public onlyOwner();
-    function withdrawSeed()public onlyOwner();
+    function setSeedStatus(bool _status) public onlyOwner(){
+        seedStatus = _status
+    };
+    function withdrawSeed() public onlyOwner(){
+        require(secondPhase, "secondPhase didnt start");
+        require(!seedStatus, "stop seedSale");
+        currency.transfer(msg.sender, currencyBalanceOf(address(this)))
+    };
 
-    function start()public onlyOwner();
-    function setSecondPhase(bool secondPhase) public onlyOwner();
-    function getUserData(address from) public view returns(uint,uint) onlyOwner();
+    function start()public onlyOwner(){
+        startDate = block.timestamp;
+        seedStatus = true;
+        //emit
+    }
+    function setSecondPhase(bool secondPhase) public onlyOwner(){
+        require(!seedStatus, "stop seedSale");
+        require(startDate > 0, "start Date error");
+        require(block.timestamp > startDate + 30 days, "start Date error");//min 30days to retire
+        secondPhase = secondPhase_;
+
+    }
+    function getUserData(address from) public view returns(uint,uint) onlyOwner(){
+        return (points[from].points, points[from].amount)
+    };
     function getAllData() public view returns(
         bool, bool, uint,uint, uint,uint, uint
-    ) onlyOwner();
+    ) onlyOwner(){
+        return (
+            seedStatus, 
+            secondPhase, 
+            totalVolume, 
+            totalPoints,
+            initialPrice, 
+            startDate, 
+            minAmount
+        );
+    }
     
-    function getPrice() public view returns(uint);
+    function getPrice() public view returns(uint){
+        uint diffTimeInSeconds = block.timestamp - startDate
+        uint toWeeks = diffTimeInSeconds / 1 weeks;
+        return (initialPrice * (100 + toWeeks)) / 100;
+    }
     function getPriceTwo() public view returns(uint);
 }
